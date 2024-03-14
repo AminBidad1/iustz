9e838251755dff4a70e8e0dcab3a22c9456f2b1d
Amin <ma.armaghan83@gmail.com>
2024-03-15 00:23:04 +0330

    1

---
+++ libs/HowStarts.hpp
@@ -0,0 +1,13 @@
+#include <chrono>
+#include <random>
+void HowItStarts() {
+    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    default_random_engine rng(seed);
+    uniform_int_distribution<int> distribution(1, 100);
+    int random_number = distribution(rng);
+    cout<<random_number<<endl;
+    if (random_number > 70) {
+        cout << "attack";
+    } else
+        cout << "shop";
+}
