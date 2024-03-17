10ab4172240a4f29132574a2ad71e42b61a3a159
Amirrez4 <haveshkiamir@gmail.com>
2024-03-17 06:00:20 +0330

    move ConsumableItem to Item and make some changes

--- libs/Item.cpp
+++ libs/Item.cpp
@@ -1,4 +1,5 @@
 #include "Item.hpp"
+#include "Character.hpp"
 
 //Item
 Item::Item(string name , int price)
@@ -48,3 +49,31 @@ int Firearms::getFirerate(){return fire_rate;}
 
 void Firearms::setMisspercent(int miss_percet){this->miss_percent=miss_percent;}
 int Firearms::getMisspercent(){return miss_percent;}
+
+//ConsumableItem
+ConsumableItem::ConsumableItem() = default;
+ConsumableItem::ConsumableItem(string name, int price) : Item(name, price) {};
+
+void ConsumableItem::setValue(int value) { this->value->setValue(value); }
+int ConsumableItem::getValue() const { return value->getValue(); }
+
+void ConsumableItem::consume() {}
+
+//ManaBooster
+void ManaBooster::consume(Human* target)
+{
+    // increasing the target's mana by the value of item
+    target->mana->restoreMana(this->getValue());
+}
+
+//Food
+void Food::consume(Character* target)
+{
+    target->hp->heal(this->getValue());
+}
+
+//Beverage
+void Beverage::consume(Character* target)
+{
+    target->target->setDamage(target->getDamage() + this->getValue());
+}
--- libs/Item.hpp
+++ libs/Item.hpp
@@ -96,3 +96,37 @@ public:
     int getMisspercent();
 
 };
+
+// ConsumableItem
+class ConsumableItem : public Item
+{
+private:
+    Value* value;
+
+public:
+    ConsumableItem();
+    ConsumableItem(string name, int price);
+
+    void setValue(int value);
+    int getValue() const;
+
+    virtual void consume();
+};
+
+class ManaBooster : public ConsumableItem
+{
+public:
+    void consume(Human* target);
+};
+
+class Food : public ConsumableItem
+{
+public:
+    void consume(Character* target);
+};
+
+class Beverage : public ConsumableItem
+{
+public:
+    void consume(Character* target);
+};
