e35582511dab110e2f2abb3f90ee852b398a3ae4
aminbidad1 <amin.200.bidad@gmail.com>
2024-03-20 13:52:55 +0330

    Add initial Character views

---
+++ libs/views.hpp
@@ -0,0 +1,33 @@
+#include "bits/stdc++.h"
+#include "Character.hpp"
+
+
+class CharacterView
+{
+protected:
+    Character* model;
+public:
+    virtual void showAttack(Character* enemy);
+    virtual void showTakeDamage();
+};
+
+class HumanView : public CharacterView
+{
+protected:
+    Human* model;
+public:
+    void showUseItem(Item* item);
+    void showAppendHP(int value);
+    void showAppendStamina(int value);
+    
+};
+
+class ZombieView : public CharacterView
+{
+protected:
+    Zombie* model;
+public:
+    void showAttack(Character* character) override;
+};
+
+class EnemyHumanView : public CharacterView {};
