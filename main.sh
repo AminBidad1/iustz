35400261a525af5abf093f7e50608e11450f7a2f
aminbidad1 <amin.200.bidad@gmail.com>
2024-03-26 20:53:01 +0330

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,4 @@
-for file in libs/*.cpp 
-    do filename=$(basename -- "$file"); filename="${filename%.*}"; make $filename.o; 
-done
+# for file in libs/*.cpp 
+#     do filename=$(basename -- "$file"); filename="${filename%.*}"; make $filename.o; 
+# done
+make main -e objects="Value.o Item.o ConsumableItem.o Character.o views.o GameManager.o"
