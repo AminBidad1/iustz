c761308c5090093f961340e84be61b8422ef471e
Amirrez4 <haveshkiamir@gmail.com>
2024-04-06 15:31:10 +0330

    handle prices1

--- libs/GameManager.cpp
+++ libs/GameManager.cpp
@@ -57,7 +57,7 @@ Item* ItemController::createItem(ItemType type)
     {
         Food* food = new Food();
         food->setName("Pizza");
-        food->setPrice(10); // TODO: Move it to shop class
+        food->setPrice(FoodPrice); // TODO: Move it to shop class
         food->setValue(15);
         item = food;
     }
@@ -65,7 +65,7 @@ Item* ItemController::createItem(ItemType type)
     {
         StaminaBooster* staminaBooster = new StaminaBooster();
         staminaBooster->setName("Stamina Booster");
-        staminaBooster->setPrice(10);
+        staminaBooster->setPrice(StaminaBoosterPrice);
         staminaBooster->setValue(15);
         item = staminaBooster;
     }
@@ -73,7 +73,7 @@ Item* ItemController::createItem(ItemType type)
     {
         Beverage* beverage = new Beverage();
         beverage->setName("PEPSI");
-        beverage->setPrice(10);
+        beverage->setPrice(BeveragePrice);
         beverage->setValue(15);
         item = beverage;
     }
@@ -81,7 +81,7 @@ Item* ItemController::createItem(ItemType type)
     {
         KitchenKnife* kitchenKnife = new KitchenKnife();
         kitchenKnife->setName("Kitchen Knife");
-        kitchenKnife->setPrice(10);
+        kitchenKnife->setPrice(KitchenKnifePrice);
         kitchenKnife->setDamage(50);
         kitchenKnife->setMiss_percent(50);
         item = kitchenKnife;
@@ -90,7 +90,7 @@ Item* ItemController::createItem(ItemType type)
     {
         Bomb* bomb = new Bomb();
         bomb->setName("Bomb");
-        bomb->setPrice(10);
+        bomb->setPrice(BombPrice);
         bomb->setDamage(300);
         bomb->setMiss_percent(30);
         item = bomb;
@@ -99,7 +99,7 @@ Item* ItemController::createItem(ItemType type)
     {
         Molotov* molotov = new Molotov();
         molotov->setName("molotov");
-        molotov->setPrice(10);
+        molotov->setPrice(MolotovPrice);
         molotov->setDamage(100);
         molotov->setMiss_percent(10);
         item = molotov;
@@ -108,7 +108,7 @@ Item* ItemController::createItem(ItemType type)
     {
         Bristle* bristle = new Bristle();
         bristle->setName("Bristle");
-        bristle->setPrice(10);
+        bristle->setPrice(BristlePrice);
         bristle->setDamage(70);
         bristle->setMiss_percent(60);
         item = bristle;
